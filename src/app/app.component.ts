e94ae0090631d25392af2eade2ad73f4741a59b4
Ben Ong <ben_ong@yahoo.com>
2021-07-06 16:29:53 -0700

    Added array for the emails

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'email-template-with-angular';
+
+  emailDate = '';
+  emailSubject = '';
+  emailContent = '';
+
+  emails = [
+    {
+      date: '3:20pm',
+      subject: 'All hands meeting',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus placeat harum explicabo, perferendis vitae alias?'
+    },
+    {
+      date: 'Mar 2, 2021',
+      subject: 'Linkedin daily feed',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo dolores amet incidunt excepturi, tempore placeat. Impedit sit ullam dolorum repellendus.'
+    },
+    {
+      date: 'Mar 3, 2021',
+      subject: 'Appointment reminder',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo dolores amet incidunt excepturi, tempore placeat. Impedit sit ullam dolorum repellendus.'
+    }
+  ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CommonModule } from '@angular/common';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule
   ],
   providers: [],
