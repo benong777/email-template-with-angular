3a1a8d2255015a1e072fb034c1dd37b2da498833
Ben Ong <ben_ong@yahoo.com>
2021-07-06 22:57:10 -0700

    Added functionality to the delete button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
   emailSubject = '';
   emailContent = '';
 
+  currEmailIndex = 0;
   emailsEmpty = false;
 
   emails = [
@@ -33,16 +34,62 @@ export class AppComponent {
   ];
 
   onDeleteEmail() {
-    // Set emailsEmpty flag before deleting
-    if (this.emails.length === 1) {
-      this.emailsEmpty = true;
+    // Max index is the length of the emails
+    let max = this.emails.length -1;
+
+    // When the selected email is at index 0
+    if (this.currEmailIndex === 0) {
+      // If max === 0, there's no more emails
+      if (max === 0) this.emailsEmpty = true;
+      else {
+        // Delete email and keep index at 0
+        this.emails.splice(this.currEmailIndex, 1);
+        this.currEmailIndex = 0;
+      }
     }
-    if (this.emails.length > 1) {
-      this.emails.splice(0, 1);
+
+    // If selected email index is > 0, delete and decrement the index
+    if (this.currEmailIndex > 0) {
+      this.emails.splice(this.currEmailIndex, 1);
+      this.currEmailIndex -= 1;
     }
+
+    // //Set emailsEmpty flag before deleting
+    // if (this.emails.length === 0) {
+    //   this.emailsEmpty = true;
+    // }
+
+    // else if (this.emails.length === 1) {
+    //   this.emails.splice(this.currEmailIndex, 1);
+    //   this.emailsEmpty = true;
+    // }
+    // else {  // this.emails.length > 1
+    //   this.emails.splice(this.currEmailIndex, 1);
+    //   if (this.currEmailIndex === 1) {
+    //     this.currEmailIndex = 0;
+    //   }
+    //   else {
+    //     this.currEmailIndex += 1;
+    //     if (this.currEmailIndex > (this.emails.length-1)) {
+    //       this.currEmailIndex = this.emails.length -1;
+    //     }
+    //   }
+    // }
+    console.log(this.currEmailIndex);
+
+    // get length
+    // max = length - 1
+    // min = 0
+
+    // if num == 0
+          // if max == 0; empty
+          // else splice, num = 0;
+    // if num > 0
+          // if num <= max, splice, num -= 1;
   }
   onEmailSelect(index: number) {
     console.log("Email selected: " + index);
+    this.currEmailIndex = index;
     this.emailDate = this.emails[index].date;
     this.emailSubject = this.emails[index].subject;
     this.emailContent = this.emails[index].content;
